6002104db67d06735716227e620559b81598b7c7
Xacarami <gabriel.plautz2018@gmail.com>
2025-04-21 00:24:24 -0300

    Unindo conhecimento

---
+++ aula5.py
@@ -0,0 +1,9 @@
+import random
+
+numero_secreto = random.randint(1, 10)
+palpite = int(input("Tente adivinhar o número de 1 a 10: "))
+
+if palpite == numero_secreto:
+    print("Parabéns! Você acertou!")
+else:
+    print(f"Errou! O número era {numero_secreto}.")
